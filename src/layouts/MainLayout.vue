1642837aa5e58a94539463ff5cfc3d460fbf0020
JaooR <85643592+JaooR@users.noreply.github.com>
2023-05-18 11:53:51 -0300

    Fist commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style=" width: 360px; height: 53px; background-color: #1976d2; top: -1px; ">
+        <q-avatar>
+          <img src="Vector.png" @click="toggleLeftDrawer" />
+        </q-avatar>
+      </q-toolbar>
+    </q-header>
+    <q-drawer v-model="leftDrawerOpen" bordered>
+      <q-list>
+        <q-item-label header>
+
+        </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="text-center text-italic">
+    <div class="flex flex-center" style="font-size: 25px;">
+      <div style="background-color: #1976D2; border-radius: 20px; width: 238px; margin-top: 15px;">
+        <div style="color: white; font-size: 18px; margin-bottom: 18px; margin-top: 19px;">
+          Histórico
+        </div>
+      </div>
+    </div>
+    <div style="margin-top: 25px;">
+      <q-table class="flex" title="" :rows="rows" :columns="columns" row-key="name" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const columns = [
+  { name: 'codigo', field: 'codigo', label: 'Codigo' },
+  { name: 'identificacao', field: 'identificacao', label: 'Identificação' },
+  { name: 'inquilino', field: 'inquilino', label: 'Inquilino' },
+  { name: 'data_de_retirada', field: 'data_de_retirada', label: 'Data de Retirada' },
+];
+const rows = [
+  {
+    codigo: '1',
+    identificacao: 'Kabum',
+    inquilino: 'Pedro',
+    data_de_retirada: '23/03/2023',
+  },
+  {
+    codigo: '2',
+    identificacao: 'Mercado Livre',
+    inquilino: 'Pedro',
+    data_de_retirada: '23/03/2023',
+  },
+];
+</script>
